efe83d3d3d6a055b568659ac36cf869a44ccc111
foresthead <dabatman@yandex.ru>
2020-04-30 19:46:03 +0600

    Revert "Sends data when switch on/off"
    This reverts commit ada44aa033960dc4674c86064a2f715bef55f7b8.

--- 代码/ACC_manager_0.2.py
+++ 代码/ACC_manager_0.2.py
@@ -1,46 +1,25 @@
 import socket
 import pickle
 
+class manager:
 
-class AC_Manager:
+    def __init__(self):
+        self.price = input("Enter the starting price:")
+        self.temp = input("Enter the starting temperature:")
+        self.air_speed = input("Enter the starting air speed")
 
-    def __init__(self, given_status):
-        self.state = given_status
-        # self.temp = input("Enter the starting temperature:")
-        # self.air_speed = input("Enter the starting air speed")
+    def monitoring(self,socket):
 
-    # output
-    def user_state_print(status):
-        if status == 1:
-            OnOffLabel = "ON"
-        else:
-            OnOffLabel = "OFF"
-        print("The current state of the AC is " + OnOffLabel)
-
-    def manager_state_print(status):
-        if status == 1:
-            OnOffLabel = "ON"
-        else:
-            OnOffLabel = "OFF"
-        print("The current state of the AC is " + OnOffLabel)
-
-    def data_recieve(self, socket):
         while True:
             # 4096 is used to pickle the data(can be revised)
             DataFromClient = socket.recv(4096)
             DataFromClient_decode = pickle.loads(DataFromClient)
-            # print(DataFromClient_decode)
-            print("this is the state:"+str(DataFromClient_decode[0]))
-            # print("this is the temp:"+str(DataFromClient_decode[1]))
-            # print("this is the airspeed:"+str(DataFromClient_decode[2]))
+            print(DataFromClient_decode)
+            print("this is the price:"+str(DataFromClient_decode[0]))
+            print("this is the temp:"+str(DataFromClient_decode[1]))
+            print("this is the airspeed:"+str(DataFromClient_decode[2]))
 
-# implements input
-def manager_input():
-    active_key=input("Enter S to SWITCH ")
-    if active_key=='S':
-        return 1
-    else:
-        return 0
+
 
 def main():
     server = socket.socket()
@@ -49,12 +28,17 @@ def main():
     server.listen()
     Client, addr = server.accept()
     if Client:
-        print("Welcome to ACC Control System! The current state of AC is OFF")
-        AC_Manager.data_recieve(AC_Manager, Client)
+        print("the AC is turned on")
+        manager.monitoring(manager, Client)
     print("the AC is turned off")
 
 
 
-# this is the main function of the program
+#this is the main function of the program
 if __name__ == '__main__':
     main()
+
+
+
+
+

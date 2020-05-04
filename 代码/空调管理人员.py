0061a81f85ea41c650e7a518f1a4baa0ebc852c5
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-05-05 02:47:07 +0800

    Update 空调管理人员.py

--- 代码/空调管理人员.py
+++ 代码/空调管理人员.py
@@ -1,1 +1,11 @@
 #这部分是由空调管理人员该角色的代码构成的谢谢大家的配合
+
+
+class AC_manager:
+  #whatever function that is construct of 
+  
+  def check(self, object):
+    #这部分函数是来监视空调
+    
+  def operateAC(self, object):
+    #这部分函数是来运行空调

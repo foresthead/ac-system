82cbbb9fd5e1d3ece3d3e3392f9c011c471ae3a8
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-03 14:01:06 +0800

    Create services.py

---
+++ 代码/管理员/services.py
@@ -0,0 +1,15 @@
+
+class services:
+
+    def __init__(self):
+        self.State = "not Ready"
+
+    #def Initialization(self):
+
+    def ChangeState(self, State):
+        self.State = State
+
+    def PrintState(self):
+        print(self.State)
+    #def GetRoomState(self):
+

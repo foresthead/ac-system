68ae26678cf7ae80adfffc480f4eaca0280a9572
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-04 17:11:12 +0800

    Update main.py

--- 代码/main.py
+++ 代码/main.py
@@ -5,3 +5,9 @@ import dispatch
 import services
 import control
 import Reception
+
+
+#生命所有的对象
+dispatch = dispatch.dispatch()          #定义调度对象dispatch
+services = services.services()          #定义服务对象services
+control = control.control()             #定义控制对象control

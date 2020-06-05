487ca55327b2b5f73ab4efc95a80a7af3932df22
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-05 16:21:56 +0800

    Update dispatch.py

--- 代码/管理员/dispatch.py
+++ 代码/管理员/dispatch.py
@@ -1,13 +1,23 @@
 class dispatch:
     def __init__(self):
-        self.State = "not Ready"
-
-    # def Initialization(self):
+        self.State = "Not Ready"
 
     def ChangeState(self, State):
         self.State = State
 
-    def PrintState(self):
-        print(self.State)
-    # def GetRoomState(self):
+    #客户端发个请求后，判断该请求是否分配服务对象还是放入等待队列
+    def dispatch(self, services, waitingQueue, request):
+
+        result = 1
+        for i in range(0,3):
+            #查看哪一个服务对象还没介绍请求
+            if services.state == "Ready":
+                services[i].accept_Service(request)
+                print("the ", request.id," request was given to ", i," service")
+                result = 1
+
+        #没找到不忙的服务对象，把请求放入到等待队列
+        if result != 0:
+            waitingQueue.insert(request)
+            print("Request ",request.id," succesfully inserted in the waitingQueue")
 

d95ff1400d32579ebd43d9da0c20e45ee8a6c959
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-05 16:27:25 +0800

    Create waitingQueue.py

---
+++ 代码/管理员/waitingQueue.py
@@ -0,0 +1,13 @@
+class waitingQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def insert(self):
+        #insert the request inside the queue
+
+    def delete(self):
+        #remove the request from the queue
+
+    def sort(self):
+        #sort the queue according to the priority

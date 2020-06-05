23a117474bd628e4e2e6f3a65269893eaa6ecab1
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-05 17:03:42 +0800

    Update waitingQueue.py

--- 代码/管理员/waitingQueue.py
+++ 代码/管理员/waitingQueue.py
@@ -3,11 +3,22 @@ class waitingQueue:
     def __init__(self):
         self.queue = []
 
-    def insert(self):
-        #insert the request inside the queue
 
-    def delete(self):
-        #remove the request from the queue
+    def insert(self,request):
+        #添加请求到等待队列
+        self.queue.append(request)
+        self.sort_by()
+        print("the ", i, "th request is succefully inserted to the queue")
 
-    def sort(self):
+
+    def delete(self,request):
+        #从等待队列删除请求
+        for i in self.queue:
+            if self.queue[i].id == request.id :
+                self.queue.remove(self.queue[i])
+        print("the ",i,"th request is succefully removed from the queue")
+
+
+    def sort_by(self):
         #sort the queue according to the priority
+        pass

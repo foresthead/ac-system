cb90c12df9a0200984a41e6c42197c8471900474
foresthead <dabatman@yandex.ru>
2020-06-05 21:47:27 +0600

    Did all the RDR part!

--- 代码/前台/Reception.py
+++ 代码/前台/Reception.py
@@ -1,37 +1,56 @@
+
 # controller will tell classes what to do
-
-
 class ReceptionController:
-    def __init__(self, InitialType):
-        self.choice = InitialType  # this attribute will tell the class, what did user choose - invoice or DR
+    def __init__(self, RDRorI, OutputType, RoomID):
+        self.RDRorI = RDRorI  # this attribute will tell the class, what did user choose - invoice or DR
+        self.OutputType = OutputType
+        self.ID = RoomID
+        self.ThisRDR = ServiceRDR(self.ID)
+        self.ThisInvoice = ServiceRDR(self.ID)
 
-    # this method contains service constructor
-    def CreateService(self):
-        if self.choice == 'RDR':
-            print("RDR service start...")
-        elif self.choice == 'I':
-            print("Invoice service start...")
-        else:
-            print("Service initializing error")
 
-    # this method will create this object
-    def Create_PrintList(self):
-        if self.choice == 'RDR':
+    # this method is the constructor of RDR and invoice.
+    def commandCreateList(self):
+        if self.RDRorI == '1':
             print("RDR processing...")
-        elif self.choice == 'I':
+            ListContent = self.ThisRDR.createList()
+            self.Print(ListContent)
+        elif self.RDRorI == '2':
             print("Invoice processing...")
+
         else:
             print("Processing error")
 
+
+
     # this method contains sends a printing command
-    def Print(self):
-        if self.choice == 'RDR':
+    def Print(self, ListContent):
+        self.ListContent = ListContent
+        if self.RDRorI == '1':
             print("Printing DR...")
-        elif self.choice == 'I':
+            Printer = RDRPrinter()
+            if self.OutputType == '1':
+                Printer.PutOnScreen(ListContent)
+            elif self.OutputType == '2':
+                Printer.PutInTxt(ListContent)
+            else:
+                print("Printer error")
+
+
+        elif self.RDRorI == '2':
             print("Printing invoice...")
+
         else:
             print('print error')
 
+class Database:
+    def __init__(self):
+        import datetime
+        self.FanSpeed = 1
+        self.FeeRate = 5
+        self.Temperature = 28
+        self.CheckInTime = datetime.datetime(2017, 2, 26)
+        self.Fee = self.FanSpeed * self.FeeRate * (self.Temperature - 25)
 
 ##################################################################################
 #                                                                                #
@@ -40,12 +59,86 @@ class ReceptionController:
 ##################################################################################
 
 
-class ServiceRDR:
-    pass
+class ServiceRDR:   # This class made for creating DR that we can output on screen or print to file.
+    def __init__(self, ID):
+        self.RoomID: int = ID
+        self.ValuesSource = Database()
+
+    # taking values using getValues, putting them to list_RDR where they are properly formatted
+    def createList(self):
+        prepareList = self.getValues()
+        ThisList = list_RDR(self.RoomID, prepareList)
+        OutputScreen = ThisList.ReadyList()
+        return OutputScreen
+        # for i in range(len(OutputScreen)):      # output every string of our list_RDR
+        #     print(OutputScreen[i])
 
 
-class list_RDR:
-    pass
+    # Getting values from Database and time method
+    def getValues(self):
+        OurTimeList = self.getRequestDuration()
+        OurTemperature = self.ValuesSource.Temperature
+        OurFanSpeed = self.ValuesSource.FanSpeed
+        OurFeeRate = self.ValuesSource.FeeRate
+        OurFee = self.ValuesSource.Fee
+        OurDateIn = OurTimeList[0]
+        OurDateOut = OurTimeList[1]
+        OurDuration = OurTimeList[2]
+        prepareList = [str(OurTemperature), str(OurFanSpeed), str(OurFeeRate), str(OurFee), OurDateIn, OurDateOut, OurDuration]
+        return prepareList
+
+
+    # does everything related to time and puts it in special list
+    def getRequestDuration(self):
+        import datetime
+        # datetime.datetime(2017, 4, 5, 0, 17, 8, 24239)
+        TimeIn = self.ValuesSource.CheckInTime
+        TimeNow = datetime.datetime.now()
+        Duration = TimeNow - TimeIn
+        TimeList = [str(TimeIn), str(TimeNow), str(Duration)]
+        return TimeList
+
+
+
+class RDRPrinter():
+    def __init__(self):
+        self.RoomID: int = RoomID
+
+    def PutOnScreen(self, ListContent):
+        for i in range(len(ListContent)):      # output every string of our list_RDR
+            print(ListContent[i])
+
+    def PutInTxt(self, ListContent):
+        import webbrowser
+        # for i in range(len(ListContent)):  # output every string of our list_RDR
+        with open('detailed_record.txt', 'w') as printfile:
+            for line in ListContent:
+                printfile.write(line + '\n')
+            # printfile.write(ListContent[i])
+            webbrowser.open("detailed_record.txt")         #opening the reciept in notepad
+
+
+class list_RDR:              # Gives us the PROPERLY FORMATTED LIST.
+    def __init__(self, RoomID, prepareList):
+        self.RoomID: int = RoomID
+        self.prepareList = prepareList
+
+    def ReadyList(self):
+        prepareList = ServiceRDR(self.RoomID)
+        prepareList.getValues()
+        StringOne = str("Your room:\t\t\t" + self.RoomID)
+        StringTwo = str("Temperature:\t\t\t" + self.prepareList[0])
+        StringThree = str("FanSpeed:\t\t\t" + self.prepareList[1])
+        StringFour = str("Fee Rate:\t\t\t" + self.prepareList[2])
+        StringFive = str("Fee:\t\t\t\t" + self.prepareList[3])
+        StringSix = ("Check In Time:\t\t" + self.prepareList[4])
+        StringSeven = ("Time Now:\t\t" + self.prepareList[5])
+        StringEight = ("Time in hotel:\t\t" + self.prepareList[6])
+        ReadyToPrint = [StringOne, StringTwo, StringThree, StringFour, StringFive, StringSix, StringSeven, StringEight]
+        return ReadyToPrint     # returns set of PROPERLY FORMATTED STRINGS.
+
+
+
 
 
 ##################################################################################
@@ -74,13 +167,14 @@ class InvoicePrinter:
 ##################################################################################
 
 
-InitialType = input("RDR or I")
-ReceptionQuery = ReceptionController(InitialType)
-ReceptionQuery.CreateService()
-UserChoice: str = input("Show on the screen or Print(1 or 2)")
-if UserChoice == ('1'):
-    ReceptionQuery.Create_PrintList()
-elif UserChoice == ('2'):
-    ReceptionQuery.Print()
-else:
-    print("error")
+RDRorI = input("RDR or I (1 or 2)\n")
+RoomID = input("Enter the room ID (1-5)\n")
+OutputType: str = input("Show on the screen or Print (1 or 2)\n")
+ReceptionQuery = ReceptionController(RDRorI, OutputType, RoomID)
+ReceptionQuery.commandCreateList()
+# if UserChoice == ('1'):
+#     ReceptionQuery.Create_PrintList()
+# elif UserChoice == ('2'):
+#     ReceptionQuery.Print()
+# else:
+#     print("error")

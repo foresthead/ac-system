feabd0d7fb4d431823d768d81b141551d86a20a2
SmithOrlando <64551828+SmithOrlando@users.noreply.github.com>
2020-06-05 22:49:01 +0800

    Update dispatch.py

--- 代码/管理员/dispatch.py
+++ 代码/管理员/dispatch.py
@@ -21,3 +21,32 @@ class dispatch:
             waitingQueue.insert(request)
             print("Request ",request.id," succesfully inserted in the waitingQueue")
 
+
+            
+ 
+class waitingQueue:
+
+    def __init__(self):
+        self.queue = []
+        self.queue_length = 0
+
+
+    def insert(self,request):
+        #添加请求到等待队列
+        self.queue.append(request)
+        self.queue_length += 1
+        self.sort_by()
+        print("the request ", request.id, " is succefully inserted to the queue")
+
+
+    def delete(self,request):
+        #从等待队列删除请求
+        for i in self.queue:
+            if self.queue[i].id == request.id :
+                self.queue.remove(self.queue[i])
+        print("the ",i,"th request is succefully removed from the queue")
+
+
+    def sort_by(self):
+        #sort the queue according to the priority
+        pass
